eede31dc16ce1ceca84e49adbd662dcc7ae06094
momehtoff <momehtoff@gmail.com>
2023-10-04 19:48:27 +0300

    scripts update

--- src/convertRusUTF8.ps1
+++ src/convertRusUTF8.ps1
@@ -1,14 +1,20 @@
 ﻿#####################################################################################################
 ### Convertor Russian symbols 2 UTF-8 for Text filter 4 SpamAssassin                              ###
-### v.20230922                                                                                    ###
+### v.20231003                                                                                    ###
 #####################################################################################################
 
 Param (
-	[string] $inputFilter = $(Read-Host -prompt "Введите значение inputFilter")
+	[string] $inputFilter = $(Read-Host -prompt "Введите значение inputFilter"),
+    [switch] $addBP,                                                               # Добавить ([[:blank:][:punct:]]?) после каждого символа
+    [switch] $codeRus2UTF8                                                         # Русские буквы кодировать в таблицу символов UTF8
 )
 
-$russian_letters = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя";
-$fileRusUTF8 = 'RusUTF8.txt';
+$letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+$no_PB_letters      = ".?|)";
+$file_convert_table = 'convert.tbl';
+$file_codesRus2UTF8 = 'Rus2UTF8.tbl';
+
+if ($codeRus2UTF8) { $file_convert_table = $file_codesRus2UTF8; }   # Кодировать по таблице UTF8
 
 if (-not $inputFilter) {
 	Write-Warning "Не введено значение параметра [inputFilter]!";
@@ -19,10 +25,10 @@ Write-Host "Длина входящей строки:" $inputFilter.Length "си
 
 $current_dir = ($MyInvocation.MyCommand.Path | split-path -parent); # Текущий каталог
 
-$check_RusUTF8 = (Test-Path -Path ($current_dir + '\' + $fileRusUTF8));
+$check_RusUTF8 = (Test-Path -Path ($current_dir + '\' + $file_convert_table));
 
 if (-not $check_RusUTF8) {
-	Write-Warning "Файл [$fileRusUTF8] не найден!"
+	Write-Warning "Файл [$file_convert_table] не найден!"
 	exit;
 }
 
@@ -42,23 +48,72 @@ if ($count_open_round_brackets -ne $count_close_round_brackets) {
 	exit;
 }
 
-# Конвертация русских букв в UTF8 кодировку
-$utf8_table = (Get-Content -Path ($current_dir + '\' + $fileRusUTF8));
+# Добавление ([[:blank:][:punct:]]?)
+#if ($addBP) {
+#    $PBFilter = "";
+#
+#    for ($idx = 0; $idx -lt $inputFilter.Length; $idx++) {
+#        $PBFilter += $inputFilter[$idx];
+#
+#        if ($letters.Contains($inputFilter[$idx])) {    # Добавляем BP после букв
+#            if (($idx + 1) -lt $inputFilter.Length) {
+#                if (-not $no_PB_letters.Contains($inputFilter[$idx + 1])) {
+#                    $PBFilter += '([[:blank:][:punct:]]?)';
+#                }
+#            }
+#        }
+#
+#        if ($inputFilter[$idx] -eq ')') {                       # Добавляем BP после круглой скобки, перед буквами
+#            if (($idx + 1) -lt $inputFilter.Length) {
+#                if ($letters.Contains($inputFilter[$idx + 1])) {
+#                    $PBFilter += '([[:blank:][:punct:]]?)';
+#                }
+#            }
+#        }
+#    }
+#
+#    if ($PBFilter.Length -gt 0) { $inputFilter = $PBFilter; }
+#}
+
+# Конвертация букв
+$convert_table = (Get-Content -Path ($current_dir + '\' + $file_convert_table));
 
 $resultFilter = "";
 
 for ($idx = 0; $idx -lt $inputFilter.Length; $idx++) {
-    if ($russian_letters.Contains($inputFilter[$idx])) {
-        foreach ($str in $utf8_table) {
+    if ($letters.Contains($inputFilter[$idx])) {
+        $replacement_found = $false;
+
+        foreach ($str in $convert_table) {
             if ($str.Contains($inputFilter[$idx])) {
                 $str_arr = $str.Split(':');
 
-                $resultFilter = $resultFilter + $str_arr[1];
+                $resultFilter += $str_arr[1];
+                $replacement_found = $true;
                 break;
             }
         }
+        
+        if (-not $replacement_found) {  # Замена не найдена
+            $resultFilter += $inputFilter[$idx];
+            Write-Warning "Не найдена замена для символа" $inputFilter[$idx];
+        }
+
+        if ($addBP) {   # Добавление ([[:blank:][:punct:]]?)
+            if (($idx + 1) -lt $inputFilter.Length) {
+                if (-not $no_PB_letters.Contains($inputFilter[$idx + 1])) { $resultFilter += '([[:blank:][:punct:]]?)'; }
+            }
+        }
     } else {
-        $resultFilter = $resultFilter + $inputFilter[$idx];
+        $resultFilter += $inputFilter[$idx];
+
+        if ($addBP) {   # Добавление ([[:blank:][:punct:]]?)
+            if ($inputFilter[$idx] -eq ')') {                       # Добавляем BP после круглой скобки, перед буквами
+                if (($idx -gt 0) -and (($idx + 1) -lt $inputFilter.Length)) {
+                    if ($letters.Contains($inputFilter[$idx + 1]) -or $inputFilter[$idx + 1] -eq '(') { $resultFilter += '([[:blank:][:punct:]]?)'; }
+                }
+            }
+        }
     }
 }
 
